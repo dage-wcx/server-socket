007bad032ba0e7b396e4daf4c1628fb001027db6
dage <1192274716@qq.com>
2019-04-30 16:47:45 +0800

    基本搭建

--- src/main/java/com/serversocket/entity/Message.java
+++ src/main/java/com/serversocket/entity/Message.java
@@ -1,6 +1,7 @@
 package com.serversocket.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @ClassName Message 消息类
@@ -10,4 +11,39 @@ import java.io.Serializable;
  * @Version
  **/
 public class Message implements Serializable {
+
+    /**
+     * 消息id
+     */
+    private Long msgId;
+
+    /**
+     * 发送消息的用户id
+     */
+    private Long sendMsgUserId;
+
+    /**
+     * 发送消息的用户名
+     */
+    private String sendMsgUsername;
+
+    /**
+     * 发送消息的用户端口
+     */
+    private Integer sendMsgUserPort;
+
+    /**
+     * 发送消息的用户端口
+     */
+    private Integer sendMsgUserIP;
+
+    /**
+     * 接收消息的用户id
+     */
+    private Long receiveMsgUserId;
+
+    /**
+     * 发送消息的时间
+     */
+    private Date sendMsgTime;
 }
--- src/main/java/com/serversocket/entity/Userinfo.java
+++ src/main/java/com/serversocket/entity/Userinfo.java
@@ -1,6 +1,7 @@
 package com.serversocket.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @ClassName Userinfo 用户类
@@ -10,4 +11,54 @@ import java.io.Serializable;
  * @Version
  **/
 public class Userinfo implements Serializable {
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 用户账号
+     */
+    private Long userAccount;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 用户头像
+     */
+    private String headPhoto;
+
+    /**
+     * 用户性别
+     */
+    private String sex;
+
+    /**
+     * 用户生日
+     */
+    private Date birthday;
+
+    /**
+     * 用户注册时间
+     */
+    private Date registerTime;
+
+    /**
+     * 用户修改时间
+     */
+    private Date modifyTime;
+
+    /**
+     * 用户标记  0-无效  1-有效
+     */
+    private Integer tag;
 }
