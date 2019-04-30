a17a46bad5d7eba8ff3fcf4b34b40ab3a3e1c3b3
dage <1192274716@qq.com>
2019-04-30 16:02:02 +0800

    socket用springboot搭建好

---
+++ src/main/java/com/serversocket/entity/Message.java
@@ -0,0 +1,13 @@
+package com.serversocket.entity;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName Message 消息类
+ * @Description
+ * @Author wcx
+ * @Date 2019/04/30 15:59
+ * @Version
+ **/
+public class Message implements Serializable {
+}
---
+++ src/main/java/com/serversocket/entity/Userinfo.java
@@ -0,0 +1,13 @@
+package com.serversocket.entity;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName Userinfo 用户类
+ * @Description
+ * @Author wcx
+ * @Date 2019/04/30 15:58
+ * @Version
+ **/
+public class Userinfo implements Serializable {
+}
