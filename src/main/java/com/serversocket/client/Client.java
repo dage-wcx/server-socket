f841398e88b0914587c4f9bbce3cecb3a8d49f58
dage <1192274716@qq.com>
2019-04-30 17:06:14 +0800

    pojo类 注册登录页

---
+++ src/main/java/com/serversocket/client/Client.java
@@ -0,0 +1,11 @@
+package com.serversocket.client;
+
+/**
+ * @ClassName Client
+ * @Description
+ * @Author wcx
+ * @Date 2019/04/30 17:05
+ * @Version
+ **/
+public class Client {
+}
---
+++ src/main/java/com/serversocket/controller/UserinfoController.java
@@ -0,0 +1,11 @@
+package com.serversocket.controller;
+
+/**
+ * @ClassName UserinfoController
+ * @Description
+ * @Author wcx
+ * @Date 2019/04/30 17:05
+ * @Version
+ **/
+public class UserinfoController {
+}
